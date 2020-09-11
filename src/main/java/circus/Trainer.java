a5b245b68aa134cae5f275b2e87d1cf27e16e9f6
e0425705 <e0425705@u.nus.edu>
2020-09-11 16:21:26 +0800

    merge issue

--- src/main/java/circus/Trainer.java
+++ src/main/java/circus/Trainer.java
@@ -3,25 +3,18 @@ package circus;
 import circus.animal.Animal;
 import circus.animal.Bird;
 import circus.animal.Duck;
+import circus.animal.Parrot;
 
 public class Trainer {
     public static void main(String[] args) {
         Duck d = new Duck();
-        getToSpeak(d);
-
+        System.out.println(d.speak());
         Bird b = (Bird)d;  // upcasting
-        getToSpeak(b);
-
         Animal a = (Animal)b; // upcasting
-        getToSpeak(a);
-
+        System.out.println(a.speak());
         Duck d2 = (Duck) a; // downcasting
         train(new Duck());
-        // train(new circus.animal.Parrot());
-    }
-
-    private static void getToSpeak(Animal animal) {
-        System.out.println(animal.speak());
+        // train(new Parrot());
     }
 
     private static void train(Bird bird) {
--- src/main/java/circus/animal/Animal.java
+++ src/main/java/circus/animal/Animal.java
@@ -3,6 +3,10 @@ package circus.animal;
 import circus.Asset;
 
 public abstract class Animal implements Asset {
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> upstream/improved
     public abstract String speak();
 }
